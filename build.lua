f0410862ea41a6070a47a91b8a25ad4ce43deb2a
Markus Kurtz <m@mgkurtz.de>
2024-11-07 23:46:30 +0100

    Add l3build regression test suite

---
+++ build.lua
@@ -0,0 +1,22 @@
+module = "pdfjam"
+
+scriptfiles = {"bin/*"}
+scriptmanfiles = {"man/*"}
+
+checkengines = {"engine"}
+pvtext = ".jam"
+
+uploadconfig = {
+	pkg = "pdfjam",
+	version = "",
+	-- announcement = "",
+	author = "David Firth; Reuben Thomas; Markus Kurtz",
+	uploader = "Markus Kurtz",
+	license = "lppl",
+	summary = "",
+	ctanPath = "/support/pdfjam",
+	repository = "https://codeberg.org/rrthomas/pdfjam",
+	bugtracker = "https://codeberg.org/rrthomas/pdfjam/issues",
+	description = "",
+	topic = {},
+}
