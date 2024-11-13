b24a0ee8ccb01046ccf5330024d4af0e36712867
Markus Kurtz <m@mgkurtz.de>
2024-11-13 10:27:13 +0100

    Fixup for moving pdfjam

--- tests/sanitizing/cmd.sh
+++ tests/sanitizing/cmd.sh
@@ -1,1 +1,1 @@
-../../bin/pdfjam --batch --vanilla ../batch-job/letters.pdf --picturecommand "\\put(20,20){Letters.\\quad What's \$9\\cdot6\$?}" --suffix actual-output --outfile .
+../../pdfjam --batch --vanilla ../batch-job/letters.pdf --picturecommand "\\put(20,20){Letters.\\quad What's \$9\\cdot6\$?}" --suffix actual-output --outfile .
--- tests/space-in-path-to-source-file/cmd.sh
+++ tests/space-in-path-to-source-file/cmd.sh
@@ -1,1 +1,1 @@
-../../bin/pdfjam --vanilla 'sub dir'/numbers-A4.pdf --nup 2x1 --frame true --landscape --outfile actual-output.pdf
+../../pdfjam --vanilla 'sub dir'/numbers-A4.pdf --nup 2x1 --frame true --landscape --outfile actual-output.pdf
