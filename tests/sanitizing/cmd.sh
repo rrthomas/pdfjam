51d296b0d2dd636bd6963ce7cf58b8a72437b17b
Markus Kurtz <m@mgkurtz.de>
2024-11-15 10:11:59 +0100

    Integrate tests folder in testfiles

--- tests/sanitizing/cmd.sh
+++
@@ -1,1 +0,0 @@
-../../bin/pdfjam --batch --vanilla ../batch-job/letters.pdf --picturecommand "\\put(20,20){Letters.\\quad What's \$9\\cdot6\$?}" --suffix actual-output --outfile .
--- tests/space-in-the-file-name/cmd.sh
+++
@@ -1,1 +0,0 @@
-../../bin/pdfjam --vanilla 'numbers A4.pdf' --paper letterpaper --nup 2x1 --frame true --landscape --suffix actual-output --outfile .
