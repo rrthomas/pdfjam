d4ac32fc8a49342708690c1a2f7c4ce6745cb721
Markus Kurtz <m@mgkurtz.de>
2024-11-27 16:28:36 +0100

    Try fixing the workflow

--- utils/sandbox.sh
+++ utils/sandbox.sh
@@ -3,9 +3,10 @@ cd "$(dirname "$0")/.."
 
 mkdir -p build/bin
 rm -f build/bin/*
-for i in awk basename cat cp cut dirname file grep iconv kpsewhich ln lualatex mkdir mv od paperconf pdfinfo pdflatex realpath rm sed tee tr tty uname xelatex; do
+for i in awk basename cat cp cut dirname extractbb file grep iconv kpsewhich ln lualatex mkdir mv od paperconf pdfinfo pdflatex realpath rm sed tee tr tty uname xelatex; do
 	ln -s "$(command -v $i)" build/bin/$i || { echo "No $i command found. Something is seriously amiss."; exit 1; }
 done
 
-rm -fr build/bin/libpaper/
+mkdir -p build/bin/libpaper
+rm -f build/bin/libpaper/*
 mv build/bin/paperconf build/bin/libpaper/
