2ca704bcca65da39bfb53490f5f3ac64ad581aac
Markus Kurtz <m@mgkurtz.de>
2024-11-27 16:35:02 +0100

    Cosmetic fix

--- utils/sandbox.sh
+++ utils/sandbox.sh
@@ -1,12 +1,11 @@
 #!/bin/sh
 cd "$(dirname "$0")/.."
 
+rm -rf build/bin
 mkdir -p build/bin
-rm -f build/bin/*
 for i in awk basename cat cp cut dirname extractbb file grep iconv kpsewhich ln lualatex mkdir mv od paperconf pdfinfo pdflatex realpath rm sed tee tr tty uname xelatex; do
 	ln -s "$(command -v $i)" build/bin/$i || { echo "No $i command found. Something is seriously amiss."; exit 1; }
 done
 
-mkdir -p build/bin/libpaper
-rm -f build/bin/libpaper/*
+mkdir build/bin/libpaper
 mv build/bin/paperconf build/bin/libpaper/
